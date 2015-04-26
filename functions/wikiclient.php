d12f8763f12fe08c3b7d6095af6ae8212fa9c30f
Mitchell Cooper <githubspam@rlygd.net>
2015-04-26 19:06:23 -0400

    login again callback

--- functions/wikiclient.php
+++ functions/wikiclient.php
@@ -3,8 +3,13 @@
 require_once(__DIR__.'/../private/config.php');
 require_once($config->wikiclient_path);
 
-// FIXME: temporarily hardcoded stuff
-require_once('/home/www/wikifier/interfaces/PHP/Wikiclient.php');
+// create wikiclient. use session ID if logged in.
 $W = new Wikiclient($config->wiki_sock, $config->wiki_name, $config->wiki_pass, isset($_SESSION['logged_in']) ? session_id() : null);
+
+// login again: redirect to login
+$W->login_again_cb = function () {
+    header('Location: /login.php'); // TODO: relative
+    die();
+};
 
 ?>
