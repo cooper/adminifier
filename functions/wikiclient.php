6b0c3e22892c50ecae4ee23f714c368db2fb4c50
Mitchell Cooper <githubspam@rlygd.net>
2015-04-26 11:33:58 -0400

    session ID in constructor

--- functions/wikiclient.php
+++ functions/wikiclient.php
@@ -5,6 +5,6 @@ require_once($config->wikiclient_path);
 
 // FIXME: temporarily hardcoded stuff
 require_once('/home/www/wikifier/interfaces/PHP/Wikiclient.php');
-$W = new Wikiclient($config->wiki_sock, $config->wiki_name, $config->wiki_pass);
+$W = new Wikiclient($config->wiki_sock, $config->wiki_name, $config->wiki_pass, isset($_SESSION['logged_in']) ? session_id() : null);
 
 ?>
