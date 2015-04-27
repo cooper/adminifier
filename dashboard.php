a33a6587a0969b88dfe29bb09b66a404dee559a1
Mitchell Cooper <githubspam@rlygd.net>
2015-04-26 21:35:51 -0400

    are

--- dashboard.php
+++ dashboard.php
@@ -4,13 +4,14 @@ require_once('functions/session.php');
 require_once('functions/wikiclient.php');
 
 $pages = $W->page_list('a+')->pages;
+print_r($pages);
 
 echo '<table>';
 foreach ($pages as $page) {
     echo
     '<tr><td>' .
-    '<a href="edit-page.php?page="'.$page['file'].'">' .
-    $page['name'] .
+    '<a href="edit-page.php?page="'.$page->file.'">' .
+    $page->name .
     '</td></tr>';
 }
 echo '</table>';
