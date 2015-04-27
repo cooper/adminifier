b3464446d54de9f664abd6d0519c8baf3f475027
Mitchell Cooper <githubspam@rlygd.net>
2015-04-27 08:56:36 -0400

    logout button

--- dashboard.php
+++ dashboard.php
@@ -5,6 +5,7 @@ require_once('functions/wikiclient.php');
 
 $pages = $W->page_list('a+')->pages;
 echo "Hi {$_SESSION['username']}<br />";
+echo '<a href="logout.php">Logout</a><br />';
 
 echo '<table>';
 foreach ($pages as $page) {
