424454836950dbd3258b69851c220f282c6a6bbf
Mitchell Cooper <githubspam@rlygd.net>
2015-04-28 22:46:44 -0400

    move page

--- edit-page.php
+++ edit-page.php
@@ -16,10 +16,28 @@ echo '<a href="functions/delete-page.php?page='.urlencode($_GET['page']).'">Dele
 
 <br />
 
+<?
+    $short_page = substr_compare(
+        $_GET['page'],
+        '.page',
+        strlen($_GET['page']) - strlen('.page'),
+        strlen($_GET['page'])
+    ) == 0 ?
+        substr($_GET['page'], 0, -strlen('.page')) :
+    $_GET['page']
+?>
+<form action="functions/move-page.php" method="post">
+    <input type="hidden" value="<?= htmlspecialchars($_GET['page']) ?>" name="page" />
+    <input type="text" value="<?= htmlspecialchars($short_page) ?>" name="new_name" />
+    <input type="submit" value="Rename" name="submit" />
+</form>
+
+<br />
+
 <form action="functions/write-page.php" method="post">
     <input type="hidden" value="<?= htmlspecialchars($_GET['page']) ?>" name="page" />
     <textarea name="content" style="font-family: monospace; width: 1000px; height: 500px;">
 <?= htmlspecialchars($result->content) ?>
     </textarea>
-    <input type="submit" name="submit" />
+    <input type="submit" name="submit" value="Save" />
 </form>
---
+++ functions/move-page.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once(__DIR__.'/session.php');
+require_once(__DIR__.'/wikiclient.php');
+
+if (!isset($_POST['page']) || !isset($_POST['new_name']))
+    die('Missing required parameters');
+
+if (!$W->move_page($_POST['page'], $_POST['new_name'])->moved)
+    die('Move failed');
+
+header('Location: ../dashboard.php');
+
+?>
