0b27c7ce1a3fb6b0e426c94601c7773fd4a348ed
Mitchell Cooper <githubspam@rlygd.net>
2015-04-29 19:31:47 -0400

    adjust account name

--- dashboard.php
+++ dashboard.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
 
 <div id="top-bar">
-    <span class="account-title"><?= $_SESSION['username'] ?></span>
+    <span class="account-title"><i class="fa fa-user"></i> <?= $_SESSION['username'] ?></span>
     <span class="wiki-title"><?= $config->wiki_name ?></span>
     <span class="page-title"><i class="fa fa-home"></i> Dashboard</span>
 </div>
@@ -29,7 +29,6 @@
 <?
 
 $pages = $W->page_list('a+')->pages;
-echo "Hi {$_SESSION['username']}<br />";
 echo '<a href="logout.php">Logout</a><br />';
 echo '<a href="create-page.php">New page</a><br />';
 
