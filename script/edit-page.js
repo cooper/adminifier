2950b9396cc314efd2af380497c62894cb46124c
Mitchell Cooper <githubspam@rlygd.net>
2015-07-19 19:41:34 -0400

    new colors

--- script/edit-page.js
+++ script/edit-page.js
@@ -70,13 +70,13 @@ function setupToolbar () {
         li.addEvent('mouseenter', function () {
             li.morph({
                 width: '100px',
-                backgroundColor: '#232323'
+                backgroundColor: '#2096ce'
             });
         });
         li.addEvent('mouseleave', function () {
             li.morph({
                 width: '15px',
-                backgroundColor: '#2096ce'
+                backgroundColor: '#006595'
             });
         });
     });
