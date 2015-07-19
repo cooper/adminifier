2cb6bf862f8eb3d175b673e00632c46636b189ff
Mitchell Cooper <githubspam@rlygd.net>
2015-07-19 12:23:40 -0400

    pretty

--- script/edit-page.js
+++ script/edit-page.js
@@ -1,5 +1,5 @@
 console.log('Edit page script loaded');
 
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/xcode");
 editor.getSession().setMode("ace/mode/plain_text");
