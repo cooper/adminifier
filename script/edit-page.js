1b0ff6272cb9fa4db51682fac405d9ffd036e8cd
Mitchell Cooper <githubspam@rlygd.net>
2015-07-19 13:05:46 -0400

    force resize

--- script/edit-page.js
+++ script/edit-page.js
@@ -6,4 +6,5 @@ editor.getSession().setMode("ace/mode/plain_text");
 
 window.addEvent('resize', function () {
     editor.setStyle('height', window.innerHeight - $('top-bar').offsetHeight + 'px');
+    editor.resize();
 });
