96ff7f0dca54c487e6a785beb5dfb367ac0bc1ec
Mitchell Cooper <githubspam@rlygd.net>
2015-07-20 16:01:36 -0400

    basic page

--- frames/create-page.php
+++ frames/create-page.php
@@ -10,13 +10,24 @@ require_once(__DIR__.'/../functions/utils.php');
 // create a random page name
 $new_page = 'untitled_'.uniqid().'.page';
 
+$_time   = time();
+$_author = $_SESSION['username'];
+
 // set fake post variables
-$_POST = array();
-$_POST['page'] = $new_page;
-$_POST['content'] = '';
+$_POST = array(
+    'page'    => $new_page,
+    'content' => <<<EOF
+@page.title:   ;
+@page.created: $_time;
+@page.atuhor:  $_author;
+
+EOF;
+);
 
 // write the blank page
+ob_start();
 require_once(__DIR__.'/../functions/write-page.php');
+ob_end_clean();
 
 // redirect to edit the new page
 header('Location: edit-page.php?page='.$new_page);
