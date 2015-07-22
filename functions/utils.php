bb02359600492d8b1eeffb41df856b279b7a208d
Mitchell Cooper <githubspam@rlygd.net>
2015-07-22 01:39:40 -0400

    json errors

--- functions/utils.php
+++ functions/utils.php
@@ -4,11 +4,14 @@ require_once(__DIR__.'/session.php');
 require_once(__DIR__.'/../private/config.php');
 require_once(__DIR__.'/wikiclient.php');
 
-function json_error ($msg = 'Unknown') {
-    die(json_encode(array(
+function json_error ($msg = 'Unknown', $other_opts) {
+    $ary = array(
         'error'   => $msg,
         'success' => false
-    )));
+    );
+    if ($other_opts)
+        $ary = array_merge($ary, $other_opts);
+    die(json_encode($ary));
 }
 
 function json_success ($other_opts) {
--- functions/write-page.php
+++ functions/write-page.php
@@ -7,7 +7,10 @@ if (!isset($_POST['content']) || !isset($_POST['page']))
 
 $res = $W->page_save($_POST['page'], $_POST['content']);
 if (!$res->saved)
-    json_error('Save failed');
+    json_error('Save failed', array(
+        'rev_error'  => $res->rev_error, 
+        'rev_errors' => $res->rev_errors
+    ));
 
 json_success(array(
     'rev_info' => $res->rev_latest
