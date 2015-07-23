3344b6fda2019ae7c8810014007acb2fba4e29a0
Mitchell Cooper <githubspam@rlygd.net>
2015-07-22 22:04:10 -0400

    post params

--- functions/delete-page.php
+++ functions/delete-page.php
@@ -2,10 +2,10 @@
 
 require_once(__DIR__.'/utils.php');
 
-if (!isset($_GET['page']))
+if (!isset($_POST['page']))
     json_error('Missing required parameters');
 
-if (!$W->page_del($_GET['page'])->deleted)
+if (!$W->page_del($_POST['page'])->deleted)
     json_error('Delete failed');
 
 json_success();
