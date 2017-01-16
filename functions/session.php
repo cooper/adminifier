257992e8b4ec40066cd55215a00d8f9d63245241
Mitchell Cooper <githubspam@rlygd.net>
2017-01-16 15:05:20 -0500

    don't destroy from session.php

--- functions/session.php
+++ functions/session.php
@@ -2,10 +2,9 @@
     require_once(__DIR__.'/../private/config.php');
     session_start();
     if (!isset($PUBLIC_PAGE) && !isset($_SESSION['logged_in'])) {
-    header('Location: '.$config->admin_root.'/login.php');
+        header('Location: '.$config->admin_root.'/login.php');
         // this must go to login.php because at this point if
         // logged_in is not set, they have been logged out already
-        session_destroy();
         die();
     }
 ?>
