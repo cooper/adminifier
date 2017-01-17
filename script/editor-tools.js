76da2d2e23d3d50ba815297c8e3ef1ba2b372549
Mitchell Cooper <githubspam@rlygd.net>
2017-01-17 15:04:38 -0500

    don't say "with errors" when it's just cuz its a draft

--- script/editor-tools.js
+++ script/editor-tools.js
@@ -333,7 +333,7 @@ function displaySaveHelper () {
 
             // something went wrong in the page display
             var displayBad = false;
-            if (!data.result || data.result.type != 'page')
+            if (!data.result || (data.result.type != 'page' && !data.draft))
                 displayBad = true;
 
             // switch to checkmark
