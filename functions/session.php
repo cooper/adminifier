26371fb375d3051ba01149904056d4796156c6f7
Mitchell Cooper <githubspam@rlygd.net>
2017-01-20 15:42:05 -0500

    need $user_info before login check

--- functions/session.php
+++ functions/session.php
@@ -1,16 +1,17 @@
 <?php
     require_once(__DIR__.'/../private/config.php');
     session_start();
+
+    $user_info = $_SESSION['user_info'];
+    if (!$user_info)
+        $user_info = new stdClass;
+
     if (!isset($PUBLIC_PAGE) && !$user_info->logged_in && !isset($API)) {
         header('Location: '.$config->admin_root.'/login.php');
         // this must go to login.php because at this point if
         // logged_in is not set, they have been logged out already
         die();
     }
-
-    $user_info = $_SESSION['user_info'];
-    if (!$user_info)
-        $user_info = new stdClass;
 
     $wiki_conf = $user_info->conf;
     if (!$wiki_conf)
