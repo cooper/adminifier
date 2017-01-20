941edeb007c53fb943e5e052ed18d06b5cbabb25
Mitchell Cooper <githubspam@rlygd.net>
2017-01-20 18:25:06 -0500

    more work on modal window

--- script/modal-window.js
+++ script/modal-window.js
@@ -17,18 +17,34 @@ var ModalWindow = new Class({
             this.setTitle(opts.title);
         if (typeof opts.icon != 'undefined')
             this.setIcon(opts.icon);
+        else
+            this.setIcon('cog');
         if (typeof opts.doneText != 'undefined')
             this.setDoneText(opts.doneText);
+        else
+            this.setDoneText('Done');
+        if (opts.padded)
+            this.content.addClass('padded');
 
         return this;
     },
 
     setTitle: function (title) {
-        this.header.innerText = title;
+        var span = this.header.getElement('span');
+        if (!span) {
+            span = new Element('span');
+            this.header.adopt(span);
+        }
+        span.innerText = title;
     },
 
     setIcon: function (iconName) {
-
+        var icon = this.header.getElement('i');
+        if (!icon) {
+            icon = new Element('i');
+            this.header.inject(icon, 'top');
+        }
+        icon.className = 'fa fa-' + iconName;
     },
 
     setDoneText: function (text) {
