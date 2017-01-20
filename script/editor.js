95c11ba7738ee7b22b5bb73ae44cc1f6f6926dcb
Mitchell Cooper <githubspam@rlygd.net>
2017-01-20 13:58:41 -0500

    debugs

--- script/editor.js
+++ script/editor.js
@@ -222,20 +222,25 @@ ae.removeLinesInRanges = function (ranges) {
         smallest = Math.min(smallest, i.start.row, i.end.row);
     });
 
+    console.log('biggest: ' + biggest);
+    console.log('smallest: ' + smallest);
+
     var lastLine;
     for (var i = biggest; i >= smallest; i--) {
 
         // if the row does not exist, this is the end of a continuous range
         if (!rows[i]) {
+            console.log('no row ' + i);
             if (typeof firstLine != 'undefined') {
+                console.log('removing lines');
                 editor.session.doc.removeLines(i + 1, lastLine);
                 firstLine = lastLine = undefined;
             }
             continue;
         }
 
-        if (typeof lastLine == 'undefined')
-            lastLine = i;
+        if (typeof lastLine == 'undefined') {
+            lastLine = i; console.log('setting lastLine: ' + i);}
     }
 };
 
