2c532e92ee49b93e8ada66d41a61c165e54c844e
Mitchell Cooper <githubspam@rlygd.net>
2017-04-20 00:29:28 -0400

    green

--- templates/adminifier-tmpl.php
+++ templates/adminifier-tmpl.php
@@ -9,5 +9,5 @@
 </script>
 
 <script type="text/x-tmpl" id="tmpl-login-check">
-    <div style="text-align: center; line-height: 60px; height: 60px;"><i class="fa fa-check-circle fa-3x center"></i></div>
+    <div id="login-window-check"><i class="fa fa-check-circle fa-3x center"></i></div>
 </script>
