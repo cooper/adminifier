98e80fd5049665047fc446e722a1198c3045d53d
Mitchell Cooper <githubspam@rlygd.net>
2017-04-20 17:31:49 -0400

    css for dash

--- frames/dashboard.php
+++ frames/dashboard.php
@@ -1,5 +1,11 @@
-<meta data-nav="dashboard" data-title="Dashboard" data-icon="home" />
-<pre>
+<meta
+    data-nav="dashboard"
+    data-title="Dashboard"
+    data-icon="home"
+    data-styles="dashboard"
+/>
+<h2>Server logs</h2>
+<pre id="server-logs">
 <?
 require_once(__DIR__.'/../functions/utils.php');
 
