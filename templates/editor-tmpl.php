e8879b9b321163298fb0e6e8e353bec98e7ad149
Mitchell Cooper <githubspam@rlygd.net>
2017-04-22 00:55:32 -0400

    less of this

--- templates/editor-tmpl.php
+++ templates/editor-tmpl.php
@@ -50,9 +50,14 @@
 
 <script type="text/x-tmpl" id="tmpl-revision-overlay">
 <div class="editor-revision-row-overlay">
-    <div class="editor-revision-diff-button" title="Compare to current"><i class="fa fa-3x fa-circle-o"></i></div>
-    <div class="editor-revision-diff-button" title="Compare to previous"><i class="fa fa-3x fa-undo"></i></div>
-    <div class="editor-revision-diff-button" title="Cancel"><i class="fa fa-3x fa-times"></i></div>
+    <div class="editor-revision-diff-button" title="Compare to current">
+        <i class="fa fa-3x fa-circle-o"></i>
+        Current
+    </div>
+    <div class="editor-revision-diff-button" title="Compare to previous">
+        <i class="fa fa-3x fa-undo"></i>
+        Previous
+    </div>
 </div>
 </script>
 
