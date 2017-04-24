68486589d78730d2658bc3218973956b7a2afaa7
Mitchell Cooper <githubspam@rlygd.net>
2017-04-24 00:19:05 -0400

    Hex title

--- templates/editor-tmpl.php
+++ templates/editor-tmpl.php
@@ -33,8 +33,14 @@
 </script>
 
 <script type="text/x-tmpl" id="tmpl-color-helper">
-    <div id="editor-color-type-hex" class="editor-color-type editor-small-tab active" title="Hex color picker"><i class="fa fa-hashtag"></i></div>
-    <div id="editor-color-type-list" class="editor-color-type editor-small-tab" title="Color list"><i class="fa fa-paint-brush"></i></div>
+    <div id="editor-color-type-hex" class="editor-color-type editor-small-tab active" title="Hex color picker">
+        <i class="fa fa-hashtag"></i>
+        HSB/RGB/Hex
+    </div>
+    <div id="editor-color-type-list" class="editor-color-type editor-small-tab" title="Color list">
+        <i class="fa fa-paint-brush"></i>
+        Colors
+    </div>
     <div id="editor-color-names"></div>
     <div id="editor-color-hex"></div>
 </script>
