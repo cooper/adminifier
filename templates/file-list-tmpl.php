2094115c533abe6955604681877679eccaae0373
Mitchell Cooper <githubspam@rlygd.net>
2017-10-22 15:29:29 -0400

    chartreuse

--- templates/file-list-tmpl.php
+++ templates/file-list-tmpl.php
@@ -7,7 +7,7 @@
     <div class="filter-row-inner">
         <input type="radio" checked /> Contains
         <input type="radio" /> Is<br />
-        <i class="fa fa-plus-circle" style="color: #00B545;"></i>
+        <i class="fa fa-plus-circle" style="color: chartreuse;"></i>
         <input type="text" />
     </div>
 </script>
