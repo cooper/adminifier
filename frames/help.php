ab291be66ceb8567a9caaac4ae140210cfe1a16f
Mitchell Cooper <githubspam@rlygd.net>
2017-10-22 09:12:51 -0400

    missing parameter

--- frames/help.php
+++ frames/help.php
@@ -13,9 +13,9 @@ if (!isset($_GET['page']))
     $_GET['page'] = '';
 
 $html = file_get_contents('http://chromeless.wikifier.notroll.net/'.$_GET['page']);
-$html = preg_replace('"/page/',     '"#!/help?page=/page/');
-$html = preg_replace('"/topic/',    '"#!/help?page=/topic/');
-$html = preg_replace('href="http',  'target="_blank" href="http');
+$html = preg_replace('"/page/',     '"#!/help?page=/page/',         $html);
+$html = preg_replace('"/topic/',    '"#!/help?page=/topic/',        $html);
+$html = preg_replace('href="http',  'target="_blank" href="http',   $html);
 
 echo $html;
 
