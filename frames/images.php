b4eab785debb950b068a019b6fe4ee22afbb068b
Mitchell Cooper <githubspam@rlygd.net>
2017-10-21 23:15:15 -0400

    more work on grid view

--- frames/images.php
+++ frames/images.php
@@ -9,9 +9,32 @@ if (isset($_GET['mode']) && $_GET['mode'] == 'list') {
 
 // grid view
 
+require_once(__DIR__.'/../functions/utils.php');
 $images = $W->image_list($sort.$order)->images;
+
 echo '<pre>'
 print_r($images);
 echo '</pre>';
 
+
 ?>
+<!--JSON
+<?= json_encode(array(
+    'results'       => $images,
+    'sort_types'    => array('a', 'c', 'u', 'm', 'd');
+)) ?>
+
+-->
+
+<meta
+    data-nav="images"
+    data-title="Images"
+    data-icon="picture-o"
+    data-buttons="image-mode"
+    data-button-image-mode="{'title': 'List view', 'icon': 'list', 'href': '#!/pages?mode=list'}"
+    data-scripts="file-list file-list/images"
+    data-styles="file-list"
+    data-flags="no-margin search buttons"
+    data-search="fileSearch"
+    data-sort="<?= $sort.$order ?>"
+/>
--- frames/pages.php
+++ frames/pages.php
@@ -52,7 +52,7 @@ switch ($list_type) {
     data-title="Images"
     data-icon="picture-o"
     data-buttons="image-mode"
-    data-button-image-mode="{'title': 'List view', 'icon': 'list', 'func': 'imageModeToggle'}"
+    data-button-image-mode="{'title': 'Grid view', 'icon': 'th', 'href': '#!/pages'}"
     data-scripts="file-list file-list/images"
 <? else: ?>
     data-nav="pages"
