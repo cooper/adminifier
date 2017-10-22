8bfc242fbc7fbcf05f850486f4518aff80efbae5
Mitchell Cooper <githubspam@rlygd.net>
2017-10-21 23:11:31 -0400

    grid view

--- frames/images.php
+++ frames/images.php
@@ -1,4 +1,17 @@
 <?
+
+// list view
+if (isset($_GET['mode']) && $_GET['mode'] == 'list') {
     $list_type = 'images';
     require_once(__DIR__.'/pages.php');
+    die();
+}
+
+// grid view
+
+$images = $W->image_list($sort.$order)->images;
+echo '<pre>'
+print_r($images);
+echo '</pre>';
+
 ?>
