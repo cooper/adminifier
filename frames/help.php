d26fe2966ecf2e838697a137835ef0101183ab03
Mitchell Cooper <githubspam@rlygd.net>
2017-10-22 09:11:46 -0400

    simple replacements for links

--- frames/help.php
+++ frames/help.php
@@ -12,6 +12,11 @@
 if (!isset($_GET['page']))
     $_GET['page'] = '';
 
-echo file_get_contents('http://chromeless.wikifier.notroll.net/'.$_GET['page']);
+$html = file_get_contents('http://chromeless.wikifier.notroll.net/'.$_GET['page']);
+$html = preg_replace('"/page/',     '"#!/help?page=/page/');
+$html = preg_replace('"/topic/',    '"#!/help?page=/topic/');
+$html = preg_replace('href="http',  'target="_blank" href="http');
+
+echo $html;
 
 ?>
