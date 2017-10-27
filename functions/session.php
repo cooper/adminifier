dc3361ef07d4fd9ab97871d1c924c7189c94ad8c
Mitchell Cooper <githubspam@rlygd.net>
2017-10-27 15:47:56 -0400

    unset even if public page or api

--- functions/session.php
+++ functions/session.php
@@ -7,15 +7,17 @@
         $user_info = new stdClass;
     $wiki_conf = isset($user_info->conf) ? $user_info->conf : new stdClass;
 
-    if (!isset($PUBLIC_PAGE) && !isset($user_info->logged_in) && !isset($API)) {
+    if (!isset($user_info->logged_in)) {
         session_unset();
         session_destroy();
-        header('Location: '.$config->admin_root.'/login.php');
-        // this must go to login.php because at this point if
-        // logged_in is not set, they have been logged out already
-        die();
+        if (!isset($PUBLIC_PAGE) && !isset($API)) {
+            header('Location: '.$config->admin_root.'/login.php');
+            // this must go to login.php because at this point if
+            // logged_in is not set, they have been logged out already
+            die();
+        }
     }
-
+    
     // fall back to wiki shortname if no name is set
     $wiki_name = isset($wiki_conf->name) ?
         $wiki_conf->name : $config->wiki_name;
